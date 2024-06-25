4d1f7ab53d54a798688590ab8f94e7324687543c
moxie <moxie.m115@gmail.com>
2024-06-25 03:18:08 +0100

    Archiving changes lol

--- Makefile
+++ Makefile
@@ -1,9 +1,26 @@
-SRCPATH := $(shell pwd)/src
-BUILDPATH := $(shell pwd)/build
-.PHONY: libds2a build_dir
+CC = $(shell which gcc)
+LD = $(shell which ld)
+CFLAGS = -Wall -Wextra -fstack-protector
+LDFLAGS = -lc -shared -z relro -z now
 
-libds2a: build_dir
-	gcc -fPIC -shared $(SRCPATH)/llist.c $(SRCPATH)/merge-sort.c $(SRCPATH)/siphash.c -o $(BUILDPATH)/libds2a.so
+CWD = $(shell pwd)
+SRC_PATH = $(CWD)/src
+INCL_PATH = $(CWD)/include
+BUILD_PATH = $(CWD)/build
 
-build_dir:
-	mkdir -p $(BUILDPATH)
+SRC_FILES = $(wildcard $(SRC_PATH)/*.c)
+OBJ_FILES = $(SRC_FILES:$(SRC_PATH)/%.c=$(BUILD_PATH)/%.o)
+
+.PHONY: clean all
+
+all: $(OBJ_FILES)
+	$(LD) $(LDFLAGS) $(OBJ_FILES) -o $(BUILD_PATH)/libds2a.so
+
+$(OBJ_FILES): $(BUILD_PATH)/%.o : $(SRC_PATH)/%.c $(BUILD_PATH)
+	$(CC) $(CFLAGS) -c $< -o $@ -I $(INCL_PATH)
+
+$(BUILD_PATH):
+	mkdir -p $@
+
+clean:
+	rm -rf $(BUILD_PATH)
--- src/merge-sort.c
+++ src/merge-sort.c
@@ -34,58 +34,58 @@ void *msort_dlist(void *dlist_hd, int (*callback)(void *, void *)){
 	if(!(split_nodes = split_dlist(dlist_hd))) return NULL;
 	while(dlist_len > 1){
 		for(i = 0; i < dlist_len; i+=2){
-				for(left = (dlist_t *)split_nodes[i]; left; left = (left) ? left->fw : NULL){
-					for(right = (dlist_t *)split_nodes[i+1]; right; right = (right) ? right->fw : NULL){
-						switch(callback(left, right)){
-							case -1:
-								fw_save = right->fw;
-                    						bk_save = right->bk;
-                    						if(right->fw) right->fw->bk = right->bk;
-                    						if(right->bk) right->bk->fw = right->fw;
+			for(left = (dlist_t *)split_nodes[i]; left; left = (left) ? left->fw : NULL){
+				for(right = (dlist_t *)split_nodes[i+1]; right; right = (right) ? right->fw : NULL){
+					switch(callback(left, right)){
+						case -1:
+							fw_save = right->fw;
+							bk_save = right->bk;
+							if(right->fw) right->fw->bk = right->bk;
+							if(right->bk) right->bk->fw = right->fw;
 
-                    						left_save = left->fw;
-                    						left->fw = right;
-                    						left->fw->bk = left;
-                    						left->fw->fw = left_save;
-                    						if(left->fw->fw) left->fw->fw->bk = left->fw;
+							left_save = left->fw;
+							left->fw = right;
+							left->fw->bk = left;
+							left->fw->fw = left_save;
+							if(left->fw->fw) left->fw->fw->bk = left->fw;
 
-                    						if(split_nodes[i+1] == right) split_nodes[i+1] = fw_save;
-                    						if(!split_nodes[i+1]){
-                        						memmove(&split_nodes[i+1], &split_nodes[i+2], (sizeof(void *)*(dlist_len-i-1)));
-                        						dlist_len--;
-                    						}
-								right = (bk_save) ? bk_save : fw_save;
-								break;
-							case 1:
+							if(split_nodes[i+1] == right) split_nodes[i+1] = fw_save;
+							if(!split_nodes[i+1]){
+								memmove(&split_nodes[i+1], &split_nodes[i+2], (sizeof(void *)*(dlist_len-i-1)));
+								dlist_len--;
+							}
+							right = (bk_save) ? bk_save : fw_save;
+							break;
+						case 1:
 
-								fw_save = right->fw;
-                    						bk_save = right->bk;
-              				      			if(right->bk) right->bk->fw = right->fw;
-                    						if(right->fw) right->fw->bk = right->bk;
-                    
-                    						left_save = left->bk;
-                    						left->bk = right;
-                    						left->bk->fw = left;
-                    						left->bk->bk = left_save;
-                			    			if(left->bk->bk) left->bk->bk->fw = left->bk;
-                    
-    			                			if(split_nodes[i] == left) split_nodes[i] = left->bk;
+							fw_save = right->fw;
+							bk_save = right->bk;
+							if(right->bk) right->bk->fw = right->fw;
+							if(right->fw) right->fw->bk = right->bk;
 
-                    						if(split_nodes[i+1] == right) split_nodes[i+1] = fw_save;
-                    						if(!split_nodes[i+1]){
-                        						memmove(&split_nodes[i+1], &split_nodes[i+2], (sizeof(void *)*(dlist_len-i-1)));
-                        						dlist_len--;
-                    						}
-								right = (bk_save) ? bk_save : fw_save;
-								break;
-							default:
-								break;
-						}
+							left_save = left->bk;
+							left->bk = right;
+							left->bk->fw = left;
+							left->bk->bk = left_save;
+							if(left->bk->bk) left->bk->bk->fw = left->bk;
+
+							if(split_nodes[i] == left) split_nodes[i] = left->bk;
+
+							if(split_nodes[i+1] == right) split_nodes[i+1] = fw_save;
+							if(!split_nodes[i+1]){
+								memmove(&split_nodes[i+1], &split_nodes[i+2], (sizeof(void *)*(dlist_len-i-1)));
+								dlist_len--;
+							}
+							right = (bk_save) ? bk_save : fw_save;
+							break;
+						default:
+							break;
 					}
 				}
+			}
 		}
 	}
-	
+
 	dlist_hd = split_nodes[0];
 	memset(split_nodes, 0, sizeof(void *)*(len_save+1));
 	free(split_nodes);
